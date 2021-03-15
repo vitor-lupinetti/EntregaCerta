3d2b921d88685f70e7879a03028c251f6a8406ac
Vitor Lupinetti <vi.lupinetti@hotmail.com>
2021-03-15 19:23:19 -0300

    alteração gitignore

---
+++ dist/server.js
@@ -0,0 +1,14 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var express_1 = __importDefault(require("express"));
+var app = express_1.default();
+var port = process.env.PORT || 3333;
+app.get('/', function (request, response) {
+    return response.json({ message: "Hello world!" });
+});
+app.listen(port, function () {
+    console.log('Server is running!!!');
+});
