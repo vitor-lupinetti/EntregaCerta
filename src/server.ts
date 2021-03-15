7798e431025b1a477df41c364ecf7ae5e36d255f
Vitor Lupinetti <vi.lupinetti@hotmail.com>
2021-03-15 19:21:50 -0300

    configs deploy

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,12 @@
 import express from 'express';
 
 const app = express();
-
+const port = process.env.PORT || 3333;
 
 app.get('/', (request, response)=> {
-    return response.json({message : "Hello world!"});
+    return response.json({message : "Entrega certa!"});
 })
 
-app.listen(3333, ()=> {
+app.listen(port, ()=> {
     console.log('Server is running!!!');
 });
