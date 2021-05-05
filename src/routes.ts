e2a26bb841ad4b5577a3168031cb8799291d7662
Nathan Reis <nathansilvareis@gmail.com>
2021-05-05 11:24:46 -0300

    Verificacao de token /confirm-delivery-delivered

--- src/routes.ts
+++ src/routes.ts
@@ -59,6 +59,6 @@ router.get("/delivery-photos/:idDelivery", ensureAuthenticatedCustomer, trimRece
 
 router.put("/delivery-delivered/:id", ensureAuthenticatedCustomer, trimReceivedValues, deliveryController.markAsDelivered);
 
-router.put("/confirm-delivery-delivered", trimReceivedValues, deliveryController.confirmDeliveryDelivered);
+router.put("/confirm-delivery-delivered", ensureAuthenticatedCustomer, trimReceivedValues, deliveryController.confirmDeliveryDelivered);
 
 export { router };
