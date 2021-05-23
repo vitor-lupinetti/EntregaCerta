e23df3352cd44bfec144cd8fd58c9593634b15c3
Nathan Reis <nathansilvareis@gmail.com>
2021-05-23 16:48:38 -0300

    Adicionado envio de e-mails apos cadastro/cancelamento de agendamento

--- src/services/MailService.ts
+++ src/services/MailService.ts
@@ -2,7 +2,9 @@ import nodemailer from "nodemailer";
 import Mail from "nodemailer/lib/mailer";
 
 import { DeliveryEntity } from "../entities/DeliveryEntity";
+import { ScheduleEntity } from "../entities/ScheduleEntity";
 import { CustomerService } from "./CustomerService";
+import DeliveryService from "./DeliveryService";
 
 export class MailService {
     private readonly EMAIL: string;
@@ -78,6 +80,59 @@ export class MailService {
 
         let subject = "Entrega com novo status";
         let mainContentMessage = `A entrega ${delivery.description} esta atualmente com o status ${delivery.status}.`;
+
+        if (buyer.email) {
+            let completeMessage = `Olá ${buyer.name},\n\n${mainContentMessage}`;
+
+            this.send(buyer.email, subject, completeMessage);
+        }
+
+        if (receiver.email) {
+            let completeMessage = `Olá ${receiver.name},\n\n${mainContentMessage}`;
+
+            this.send(receiver.email, subject, completeMessage);
+        }
+    }
+
+    public async noticeNewSchedule(schedule: ScheduleEntity) {
+        let customerService = new CustomerService();
+        let deliveryService = new DeliveryService();
+
+        let delivery = await deliveryService.findOne({ where: { id: schedule.idDelivery } });
+
+        let buyer = await customerService.findOne({ where: { id: delivery.idBuyer } });
+        let receiver = await customerService.findOne({ where: { id: delivery.idReceiver } });
+
+        let subject = "Novo agendamento feito";
+        let mainContentMessage = `A entrega ${delivery.description} recebeu uma agendamento para entrega.\n\n`;
+        mainContentMessage += `Local: ${schedule.place}\n`;
+        mainContentMessage += `Data/Hora: ${schedule.dateTimeObj.toLocaleString("pt-br")}`;
+
+        if (buyer.email) {
+            let completeMessage = `Olá ${buyer.name},\n\n${mainContentMessage}`;
+
+            this.send(buyer.email, subject, completeMessage);
+        }
+
+        if (receiver.email) {
+            let completeMessage = `Olá ${receiver.name},\n\n${mainContentMessage}`;
+
+            this.send(receiver.email, subject, completeMessage);
+        }
+    }
+
+    public async noticeCanceledSchedule(schedule: ScheduleEntity) {
+        let customerService = new CustomerService();
+        let deliveryService = new DeliveryService();
+
+        let delivery = await deliveryService.findOne({ where: { id: schedule.idDelivery } });
+
+        let buyer = await customerService.findOne({ where: { id: delivery.idBuyer } });
+        let receiver = await customerService.findOne({ where: { id: delivery.idReceiver } });
+
+        let subject = "Agendamento cancelado";
+        let mainContentMessage = `A entrega ${delivery.description} teve seu agendamento cancelado.\n\n`;
+        mainContentMessage += `Motivo: ${schedule.reason}`;
 
         if (buyer.email) {
             let completeMessage = `Olá ${buyer.name},\n\n${mainContentMessage}`;
--- src/services/ScheduleService.ts
+++ src/services/ScheduleService.ts
@@ -2,6 +2,7 @@ import { getCustomRepository } from "typeorm";
 
 import { ScheduleEntity } from "../entities/ScheduleEntity";
 import { ScheduleRepository } from "../repositories/ScheduleRepository";
+import { MailService } from "./MailService";
 import { GenericService } from "./Service";
 import { ScheduleValidation } from "./validations/ScheduleValidation";
 
@@ -10,7 +11,17 @@ export class ScheduleService extends GenericService<ScheduleEntity> {
         super(getCustomRepository(ScheduleRepository), new ScheduleValidation());
     }
 
-    public async cancel(id: string, reason: string) {
+    public async create(schedule: ScheduleEntity): Promise<ScheduleEntity> {
+        let scheduleCreated = await super.create(schedule);
+
+        let mailService = new MailService();
+
+        mailService.noticeNewSchedule(scheduleCreated);
+
+        return scheduleCreated;
+    }
+
+    public async cancel(id: string, reason: string): Promise<void> {
         const scheduleFound = await this.findOne({ where: { id } });
 
         (this.validation as ScheduleValidation).validateCancel(scheduleFound, reason);
@@ -18,5 +29,9 @@ export class ScheduleService extends GenericService<ScheduleEntity> {
         scheduleFound.reason = reason;
 
         await this.repository.save(scheduleFound);
+
+        let mailService = new MailService();
+
+        mailService.noticeCanceledSchedule(scheduleFound);
     }
 }
